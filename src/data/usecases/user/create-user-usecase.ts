deaf58aef2075c8d2cbb6bafd4dddeb26467f095
Kauam Scalzer <scalzerkauam@gmail.com>
2023-11-06 01:19:14 -0300

    feat: ensure CreateUserUsecase call IGetOneUserByEmailRepository with correct email

--- src/data/usecases/user/create-user-usecase.ts
+++ src/data/usecases/user/create-user-usecase.ts
@@ -1,15 +1,17 @@
 import { ICreateUserUsecase, CreateUserModel } from '@/domain/usecases/user'
 import { UserModel } from '@/domain/models'
 import { Hasher } from '@/data/protocols/criptography'
-import { ICreateUserRepository } from '@/data/protocols/user'
+import { ICreateUserRepository, IGetOneUserByEmailRepository } from '@/data/protocols/user'
 
 export class CreateUserUsecase implements ICreateUserUsecase {
   constructor (
     private readonly hasher: Hasher,
-    private readonly createUserRepository: ICreateUserRepository
+    private readonly createUserRepository: ICreateUserRepository,
+    private readonly getOneUserByEmailRepository: IGetOneUserByEmailRepository
   ) {}
 
   async create (data: CreateUserModel): Promise<UserModel> {
+    await this.getOneUserByEmailRepository.getOne(data.email)
     const hashedPassword = await this.hasher.hash(data.password)
     const result = await this.createUserRepository.create(Object.assign({}, data, { password: hashedPassword }))
     return result
