a2eea8e2b015569544e3c1651b2407f72ae2975b
Kauam Scalzer <scalzerkauam@gmail.com>
2023-11-11 15:02:26 -0300

    test: ensure CreateCourseRepository create an course on sucess

---
+++ src/infra/db/repositories/course/create-course-repository.spec.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm'
+import { TypeormHelper } from '@/infra/db/helpers'
+import { CreateCourseRepository } from './create-course-repository'
+
+const makeSut = (): CreateCourseRepository => {
+  return new CreateCourseRepository()
+}
+
+describe('CreateCourseRepository', () => {
+  beforeAll(async () => {
+    await TypeormHelper.connect()
+  })
+
+  afterEach(async () => {
+    await TypeormHelper.clear('course')
+  })
+
+  afterAll(async () => {
+    await TypeormHelper.desconnect()
+  })
+
+  test('Should create a course on sucess', async () => {
+    const sut = makeSut()
+    await sut.create({
+      name: 'any_name',
+      description: 'any_description',
+      author: 'any_author',
+      thumb: 'any_thumb'
+    })
+    const count = await getRepository('course').count()
+    expect(count).toBe(1)
+  })
+})
