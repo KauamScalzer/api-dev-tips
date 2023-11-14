a751bc3b0323c29a8fe9135b4402247f435265a6
Lucas Prates <lucas.prates@matrixdobrasil.com.br>
2023-11-14 11:28:52 -0300

    test: add CreateLesson usecase tests

---
+++ src/data/usecases/lesson/create-lesson.spec.ts
@@ -0,0 +1,46 @@
+import { ICreateLessonRepository, CreateLessonParams } from '@/data/protocols/lesson'
+import { CreateLesson } from './create-lesson'
+
+const makeCreateLessonRepository = (): ICreateLessonRepository => {
+  class CreateLessonRepositoryStub implements ICreateLessonRepository {
+    async create (data: CreateLessonParams): Promise<void> {}
+  }
+  return new CreateLessonRepositoryStub()
+}
+
+interface SutTypes {
+  sut: CreateLesson
+  createLessonRepositoryStub: ICreateLessonRepository
+}
+
+const makeSut = (): SutTypes => {
+  const createLessonRepositoryStub = makeCreateLessonRepository()
+  const sut = new CreateLesson(createLessonRepositoryStub)
+  return {
+    sut,
+    createLessonRepositoryStub
+  }
+}
+
+const makeFakeData = (): CreateLessonParams => ({
+  courseId: 1,
+  name: 'any_name',
+  description: 'any_description',
+  urlVideo: 'any_url_video'
+})
+
+describe('CreateLesson usecase', () => {
+  test('Should call ICreateLessonRepository with correct values', async () => {
+    const { sut, createLessonRepositoryStub } = makeSut()
+    const encryptSpy = jest.spyOn(createLessonRepositoryStub, 'create')
+    await sut.create(makeFakeData())
+    expect(encryptSpy).toHaveBeenCalledWith(makeFakeData())
+  })
+
+  test('Should throw if ICreateLessonRepository throws', async () => {
+    const { sut, createLessonRepositoryStub } = makeSut()
+    jest.spyOn(createLessonRepositoryStub, 'create').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.create(makeFakeData())
+    await expect(promise).rejects.toThrow()
+  })
+})
