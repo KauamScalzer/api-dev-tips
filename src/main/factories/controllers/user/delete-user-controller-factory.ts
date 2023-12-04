3b44a4724ae9f724606332ef322dc77d98125759
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 02:30:53 -0300

    feat: add makeDeleteUserController

---
+++ src/main/factories/controllers/user/delete-user-controller-factory.ts
@@ -0,0 +1,15 @@
+import { DeleteUserController } from '@/presentation/controllers/user'
+import { DeleteUser } from '@/data/usecases/user'
+import { DeleteUserRepository } from '@/infra/db/repositories/user'
+import { CreateLogErrorRepository } from '@/infra/db/repositories/log-error'
+import { Controller } from '@/presentation/protocols'
+import { LogControllerDecorator } from '@/main/decorators'
+import { makeDeleteUserValidation } from '@/main/factories/validations/user'
+
+export const makeDeleteUserController = (): Controller => {
+  const deleteUserRepository = new DeleteUserRepository()
+  const deleteUser = new DeleteUser(deleteUserRepository)
+  const createUserController = new DeleteUserController(deleteUser, makeDeleteUserValidation())
+  const createLogErrorRepository = new CreateLogErrorRepository()
+  return new LogControllerDecorator(createUserController, createLogErrorRepository)
+}
--- src/main/factories/controllers/user/index.ts
+++ src/main/factories/controllers/user/index.ts
@@ -1,3 +1,4 @@
 export { makeCreateUserController } from './create-user-controller-factory'
 export { makeUserAuthenticationController } from './user-authentication-controller-factory'
 export { makeUpdateUserController } from './update-user-controller-factory'
+export { makeDeleteUserController } from './delete-user-controller-factory'
