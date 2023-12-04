fdf429313740ed5fae43c4b8d60b5774024c324b
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 03:42:29 -0300

    feat: add DeleteCourseRepository

---
+++ src/infra/db/repositories/course/delete-user-repository.ts
@@ -0,0 +1,10 @@
+import { IDeleteCourseRepository } from '@/data/protocols/course'
+import { Course } from '@/infra/db/typeorm/models'
+import { getRepository } from 'typeorm'
+
+export class DeleteCourseRepository implements IDeleteCourseRepository {
+  async delete (data: number): Promise<void> {
+    const repository = getRepository(Course)
+    await repository.delete(data)
+  }
+}
--- src/infra/db/repositories/course/index.ts
+++ src/infra/db/repositories/course/index.ts
@@ -1,3 +1,4 @@
 export { CreateCourseRepository } from './create-course-repository'
 export { GetAllCourseRepository } from './get-all-course-repository'
 export { UpdateCourseRepository } from './update-course-repository'
+export { DeleteCourseRepository } from './delete-user-repository'
