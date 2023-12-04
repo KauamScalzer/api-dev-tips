3c7791238ba40ff7dcd712f13a627e0fef128d90
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 11:13:30 -0300

    feat: add GetAllCourseByUserRepository

---
+++ src/infra/db/repositories/course/get-all-course-by-user-repository.ts
@@ -0,0 +1,20 @@
+import { IGetAllCourseByUserRepository } from '@/data/protocols/course'
+import { Course } from '@/infra/db/typeorm/models'
+import { getRepository } from 'typeorm'
+
+export class GetAllCourseByUserRepository implements IGetAllCourseByUserRepository {
+  async getAll (params: IGetAllCourseByUserRepository.Params): Promise<any> {
+    const repository = getRepository(Course)
+    const [data, count] = await repository.findAndCount({
+      where: {
+        id: params.id
+      },
+      order: {
+        id: 'ASC'
+      },
+      take: params.take,
+      skip: params.skip
+    })
+    return { data, count }
+  }
+}
