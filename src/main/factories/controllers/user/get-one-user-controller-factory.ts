b92121e52e9569c166e01ce515b797ba11bdbea4
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 02:44:08 -0300

    feat: add makeGetOneUserController

---
+++ src/main/factories/controllers/user/get-one-user-controller-factory.ts
@@ -0,0 +1,15 @@
+import { GetOneUserController } from '@/presentation/controllers/user'
+import { GetOneUser } from '@/data/usecases/user'
+import { GetOneUserRepository } from '@/infra/db/repositories/user'
+import { CreateLogErrorRepository } from '@/infra/db/repositories/log-error'
+import { Controller } from '@/presentation/protocols'
+import { LogControllerDecorator } from '@/main/decorators'
+import { makeGetOneUserValidation } from '@/main/factories/validations/user'
+
+export const makeGetOneUserController = (): Controller => {
+  const getOneUserRepository = new GetOneUserRepository()
+  const getOneUser = new GetOneUser(getOneUserRepository)
+  const getOneUserController = new GetOneUserController(getOneUser, makeGetOneUserValidation())
+  const createLogErrorRepository = new CreateLogErrorRepository()
+  return new LogControllerDecorator(getOneUserController, createLogErrorRepository)
+}
---
+++ src/main/factories/validations/user/get-one-user-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationComposite, RequiredFieldValidation } from '@/validators'
+import { Validation } from '@/presentation/protocols'
+
+export const makeGetOneUserValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['id']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
