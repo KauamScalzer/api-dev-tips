235ff03740274c8fa72142b34256a6ee7fbe91b7
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 02:28:15 -0300

    feat: add DeleteUserRepository

---
+++ src/infra/db/repositories/user/delete-user-repository.ts
@@ -0,0 +1,10 @@
+import { IDeleteUserRepository } from '@/data/protocols/user'
+import { User } from '@/infra/db/typeorm/models'
+import { getRepository } from 'typeorm'
+
+export class DeleteUserRepository implements IDeleteUserRepository {
+  async delete (data: number): Promise<void> {
+    const repository = getRepository(User)
+    await repository.delete(data)
+  }
+}
--- src/infra/db/repositories/user/index.ts
+++ src/infra/db/repositories/user/index.ts
@@ -1,3 +1,4 @@
 export { CreateUserRepository } from './create-user-repository'
 export { UpdateUserRepository } from './update-user-repository'
 export { GetOneUserByEmailRepository } from './get-one-user-by-email-repository'
+export { DeleteUserRepository } from './delete-user-repository'
