e14a79a2a432dbce7dbea3b8958dcae537908874
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 02:22:07 -0300

    feat: add DeleteUserController

--- src/domain/usecases/user/index.ts
+++ src/domain/usecases/user/index.ts
@@ -1,3 +1,4 @@
 export { ICreateUser, CreateUserModel } from './create-user'
 export { IUserAuthentication, UserAuthenticationModel } from './user-authentication'
 export { IUpdateUser, UpdateUserModel } from './update-user'
+export { IDeleteUser } from './delete-user'
---
+++ src/presentation/controllers/user/delete-user-controller.ts
@@ -0,0 +1,25 @@
+import { HttpRequest, HttpResponse, Controller, Validation } from '@/presentation/protocols'
+import { badRequest, serverError, noContent } from '@/presentation/helpers/http'
+import { IDeleteUser } from '@/domain/usecases/user'
+
+export class DeleteUserController implements Controller {
+  constructor (
+    private readonly deleteUser: IDeleteUser,
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.params)
+      if (error) {
+        return badRequest(error)
+      }
+      const params = httpRequest.params
+      await this.deleteUser.delete(params.id)
+      return noContent()
+    } catch (error: any) {
+      console.log(error)
+      return serverError(error)
+    }
+  }
+}
