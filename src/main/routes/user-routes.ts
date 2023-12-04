9784acb41c35d83b49486df286f6f1b4084355cd
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-04 02:33:29 -0300

    feat: add delete user route

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { makeCreateUserController, makeUserAuthenticationController, makeUpdateUserController } from '../factories/controllers/user'
+import { makeCreateUserController, makeUserAuthenticationController, makeUpdateUserController, makeDeleteUserController } from '../factories/controllers/user'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/user', adaptRoute(makeCreateUserController()))
   router.post('/user/authentication', adaptRoute(makeUserAuthenticationController()))
   router.put('/user/:id', adaptRoute(makeUpdateUserController()))
+  router.delete('/user/:id', adaptRoute(makeDeleteUserController()))
 }
