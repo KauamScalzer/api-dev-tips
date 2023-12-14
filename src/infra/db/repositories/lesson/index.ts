fe5e0beb762dceeebeb739b56c50344a7706d719
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-14 00:41:28 -0300

    fix: correct file name

--- src/infra/db/repositories/lesson/index.ts
+++ src/infra/db/repositories/lesson/index.ts
@@ -1,2 +1,2 @@
 export { CreateLessonRepository } from './create-lesson-repository'
-export { UpdateLessonRepository } from './update-course-repository'
+export { UpdateLessonRepository } from './update-lesson-repository'
