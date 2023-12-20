c20cfa91c1e984214b7a68038f76f1b4e2ba00e4
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-20 00:32:12 -0300

    feat: add delete comment route

--- src/main/routes/comment-routes.ts
+++ src/main/routes/comment-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { makeCreateCommentController, makeGetAllCommentsByLessonController } from '../factories/controllers/comment'
+import { makeCreateCommentController, makeGetAllCommentsByLessonController, makeDeleteCommentController } from '../factories/controllers/comment'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/comment', adaptRoute(makeCreateCommentController()))
   router.get('/comment/:lessonId', adaptRoute(makeGetAllCommentsByLessonController()))
+  router.delete('/comment/:id', adaptRoute(makeDeleteCommentController()))
 }
