44986d0683aca62ed19152492981fc40359e0f52
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-19 21:55:28 -0300

    feat: add update lesson route

--- src/main/routes/lesson-routes.ts
+++ src/main/routes/lesson-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { makeCreateLessonController } from '../factories/controllers/lesson'
+import { makeCreateLessonController, makeUpdateLessonController } from '../factories/controllers/lesson'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/lesson', adaptRoute(makeCreateLessonController()))
+  router.put('/lesson/:id', adaptRoute(makeUpdateLessonController()))
 }
