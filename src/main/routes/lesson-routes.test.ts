c71b13c9db6378b7bbac088643e43d9d2ea4bafa
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-23 20:51:41 -0300

    test: add lesson route tests

--- src/main/routes/lesson-routes.test.ts
+++ src/main/routes/lesson-routes.test.ts
@@ -27,14 +27,81 @@ describe('Lesson Routes', () => {
         thumb: 'any_thumb'
       })
       await request(app).post('/api/lesson')
-        .send(
-          {
-            courseId: 1,
-            name: 'any_name',
-            description: 'any_description',
-            urlVideo: 'any_url_video'
-          }
-        )
+        .send({
+          courseId: 1,
+          name: 'any_name',
+          description: 'any_description',
+          urlVideo: 'any_url_video'
+        })
+        .expect(204)
+    })
+  })
+
+  describe('PUT /lesson', () => {
+    test('Should return 204 on sucess', async () => {
+      await getRepository('course').save({
+        id: 1,
+        name: 'any_name',
+        description: 'any_description',
+        author: 'any_author',
+        thumb: 'any_thumb'
+      })
+      await getRepository('lesson').save({
+        id: 1,
+        courseId: 1,
+        name: 'any_name',
+        description: 'any_description',
+        urlVideo: 'any_url_video'
+      })
+      await request(app).put('/api/lesson/1')
+        .send({
+          courseId: 1,
+          name: 'updated_name',
+          description: 'updated_description',
+          urlVideo: 'updated_url_video'
+        })
+        .expect(204)
+    })
+  })
+
+  describe('GET /lesson/by-course', () => {
+    test('Should return 200 on sucess', async () => {
+      await getRepository('course').save({
+        id: 1,
+        name: 'any_name',
+        description: 'any_description',
+        author: 'any_author',
+        thumb: 'any_thumb'
+      })
+      await getRepository('lesson').save({
+        id: 1,
+        courseId: 1,
+        name: 'any_name',
+        description: 'any_description',
+        urlVideo: 'any_url_video'
+      })
+      await request(app).get('/api/lesson/by-course/1?skip=1&take=1')
+        .expect(200)
+    })
+  })
+
+  describe('DELETE /lesson', () => {
+    test('Should return 204 on sucess', async () => {
+      await getRepository('course').save({
+        id: 1,
+        name: 'any_name',
+        description: 'any_description',
+        author: 'any_author',
+        thumb: 'any_thumb'
+      })
+      await getRepository('lesson').save({
+        id: 1,
+        courseId: 1,
+        name: 'any_name',
+        description: 'any_description',
+        urlVideo: 'any_url_video'
+      })
+      await request(app).delete('/api/lesson/1')
         .expect(204)
     })
   })
