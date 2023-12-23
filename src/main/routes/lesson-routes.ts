0c45c66e491f37d6c55e9028fc8054e9cf2e8eb9
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-23 15:28:02 -0300

    feat: add get all lesson by course route

--- src/main/routes/lesson-routes.ts
+++ src/main/routes/lesson-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { makeCreateLessonController, makeUpdateLessonController } from '../factories/controllers/lesson'
+import { makeCreateLessonController, makeUpdateLessonController, makeGetAllLessonByCourseController } from '../factories/controllers/lesson'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/lesson', adaptRoute(makeCreateLessonController()))
   router.put('/lesson/:id', adaptRoute(makeUpdateLessonController()))
+  router.get('/lesson/by-course/:courseId', adaptRoute(makeGetAllLessonByCourseController()))
 }
