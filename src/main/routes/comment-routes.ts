14abf01588cfe1c10deea00887aa3f950351271f
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-23 15:05:20 -0300

    feat: add update comment route

--- src/main/routes/comment-routes.ts
+++ src/main/routes/comment-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { makeCreateCommentController, makeGetAllCommentsByLessonController, makeDeleteCommentController } from '../factories/controllers/comment'
+import { makeCreateCommentController, makeGetAllCommentsByLessonController, makeDeleteCommentController, makeUpdateCommentController } from '../factories/controllers/comment'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/comment', adaptRoute(makeCreateCommentController()))
   router.get('/comment/:lessonId', adaptRoute(makeGetAllCommentsByLessonController()))
   router.delete('/comment/:id', adaptRoute(makeDeleteCommentController()))
+  router.put('/comment/:id', adaptRoute(makeUpdateCommentController()))
 }
