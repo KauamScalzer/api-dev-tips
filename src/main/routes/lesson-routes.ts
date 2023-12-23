a31c228ecef6e8b0edd02874e09d11110d339b20
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-23 15:46:04 -0300

    feat: add delete lesson route

--- src/main/routes/lesson-routes.ts
+++ src/main/routes/lesson-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { makeCreateLessonController, makeUpdateLessonController, makeGetAllLessonByCourseController } from '../factories/controllers/lesson'
+import { makeCreateLessonController, makeUpdateLessonController, makeGetAllLessonByCourseController, makeDeleteLessonController } from '../factories/controllers/lesson'
 import { adaptRoute } from '../adapters/express'
 
 export default (router: Router): void => {
   router.post('/lesson', adaptRoute(makeCreateLessonController()))
   router.put('/lesson/:id', adaptRoute(makeUpdateLessonController()))
   router.get('/lesson/by-course/:courseId', adaptRoute(makeGetAllLessonByCourseController()))
+  router.delete('/lesson/:id', adaptRoute(makeDeleteLessonController()))
 }
