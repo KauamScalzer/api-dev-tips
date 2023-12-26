82da18fc267df498aefe24549596038370a97d6f
Kauam Scalzer <scalzerkauam@gmail.com>
2023-12-26 19:03:58 -0300

    fix: correct cors

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-controll-allow-origin', '*')
-  res.set('access-controll-allow-methods', '*')
-  res.set('access-controll-allow-headers', '*')
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-headers', '*')
   next()
 }
