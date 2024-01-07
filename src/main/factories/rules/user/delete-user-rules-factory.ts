77e3b621f6b184757af6fc97d31da946efbe0529
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-01-07 14:29:46 -0300

    feat: add id as haveToExist field in delete user rules

--- src/main/factories/rules/user/delete-user-rules-factory.ts
+++ src/main/factories/rules/user/delete-user-rules-factory.ts
@@ -1,5 +1,7 @@
+import { User } from '@/infra/db/typeorm/models'
 import { Validators } from '@/presentation/protocols/validators'
 
 export const makeDeleteUserRules = (): Validators => ({
-  requiredFields: ['id']
+  requiredFields: ['id'],
+  haveToExist: [{ fieldName: 'id', model: User }]
 })
