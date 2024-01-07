cc0f721e0c2688c8124081f916e5c948567df978
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-01-07 15:15:42 -0300

    test: add id as haveToExist field in GetOneUserRules

--- src/main/factories/rules/user/get-one-user-rules-factory.spec.ts
+++ src/main/factories/rules/user/get-one-user-rules-factory.spec.ts
@@ -1,3 +1,4 @@
+import { User } from '@/infra/db/typeorm/models'
 import { makeGetOneUserRules } from '.'
 import { Validators } from '@/presentation/protocols/validators'
 
@@ -5,5 +6,6 @@ describe('makeGetOneUserRules', () => {
   test('should return correct get one user rules', () => {
     const result: Validators = makeGetOneUserRules()
     expect(result.requiredFields).toEqual(['id'])
+    expect(result.haveToExist).toEqual([{ fieldName: 'id', model: User }])
   })
 })
